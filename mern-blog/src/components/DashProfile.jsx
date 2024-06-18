9d2da944c9d47b2ac7680143d22efd3ed2fdb4d3
TaichiKarna <taichikarna@gmail.com>
2024-06-18 09:51:12 +0530

    done with uploading image

---
+++ mern-blog/src/components/DashProfile.jsx
@@ -0,0 +1,123 @@
+import {useSelector} from 'react-redux';
+import {TextInput, Button, Alert} from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase';
+import  {CircularProgressbar} from 'react-circular-progressbar';
+
+export default function DashProfile() {
+
+  const {currentUser} = useSelector(state => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
+  useEffect( () => {
+    if(imageFile){
+      uploadImageFile();
+    }
+  },[imageFile]);
+
+  
+  const uploadImageFile = async() => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = snapshot.bytesTransferred/snapshot.totalBytes * 100;
+        setImageFileUploadingProgress(progress.toFixed(0))
+      },
+      (error) => {
+        setImageFileUploadError('Could not upload image (image size must be less than 2MB)');
+        setImageFile(null);
+        setImageFileUploadingProgress(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then( (downloadUrl) => {
+          setImageFileUrl(downloadUrl);
+        })
+      }
+    )
+  }
+
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input type='file' accept='images/*' onChange={handleImageChange} ref={filePickerRef} hidden></input>
+        <div className="relative w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden"
+          onClick={() => filePickerRef.current.click()}>
+          {
+            imageFileUploadingProgress && (
+              <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%`}
+              strokeWidth={4}
+              styles={{
+                root:{
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+
+                },
+                path:{
+                  stroke: `rgba(62,152,199,${imageFileUploadingProgress/100})`,
+                },
+
+              }}
+              />
+            )
+          }
+          <img src={imageFileUrl || currentUser.profilePicture} alt="user"
+           className={`rounded-full
+            w-full h-full border-8 border-[lightgray] object-cover
+          ${imageFileUploadingProgress && imageFileUploadingProgress < 100 && 'opacity-60'}
+          `}/>
+        </div>
+        {imageFileUploadError &&   <Alert color={'failure'}>{imageFileUploadError}</Alert>
+      }
+        <TextInput
+          type='text'
+          id='username'
+          placeholder='username'
+          defaultValue={currentUser.username}
+          />
+          <TextInput
+          type='email'
+          id='email'
+          placeholder='email'
+          defaultValue={currentUser.email}
+          />
+          <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
+          defaultValue="**********"
+          />
+          <Button type='submit' gradientDuoTone={'purpleToBlue'} outline>
+            Update
+          </Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  )
+}
---
+++ mern-blog/src/components/DashSidebar.jsx
@@ -0,0 +1,35 @@
+import {Sidebar} from 'flowbite-react';
+import {HiUser, HiArrowSmRight} from 'react-icons/hi'
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState(''); 
+  
+    useEffect( () => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if(tabFromUrl){
+        setTab(tabFromUrl);
+      }
+    },[location.search]);
+  
+    return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to={'/dashboard?tab=profile'}>
+                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'
+                    as='div'>
+                        Profile
+                    </Sidebar.Item> 
+                </Link>
+                <Sidebar.Item active={true} icon={HiArrowSmRight} >
+                    Sign Out
+                </Sidebar.Item> 
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
