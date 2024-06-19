6f7f069c88e7f3594b0afc6ac9ef2b5382f84ecc
TaichiKarna <taichikarna@gmail.com>
2024-06-20 00:04:14 +0530

    get post route

--- mern-backend/routes/post.route.js
+++ mern-backend/routes/post.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const verifyUser = require('../utils/verifyUser')
-const {createPost} = require('../controllers/post.controller');
+const {createPost, getPosts} = require('../controllers/post.controller');
 
 router.post('/create',verifyUser,createPost);
+router.get('/getposts',getPosts);
 
 module.exports = router;
