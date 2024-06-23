eced5696bb83711cbc437a2c2b958c080c2d55ed
TaichiKarna <taichikarna@gmail.com>
2024-06-23 13:26:17 +0530

    fixed path issues

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ app.listen(3000,() => {
     console.log("listening to this server")
 })
 
+const __location = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
@@ -29,10 +30,10 @@ app.use("/api/auth",authRouter)
 app.use("/api/post",postRouter);
 app.use("/api/comment",commentRouter)
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__location,'/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+    res.sendFile(path.join(__location,'client','dist','index.html'));
 })
 
 app.use((err, req, res, next) => {
